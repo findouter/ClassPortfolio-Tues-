a4875733400a31dd777b6efccab401cfda9a6697
Mary Fairfield <findouter8@gmail.com>
2024-01-09 19:42:53 -0500

    first commit message: added render template import, root directory is now routed to the index page. added index.html.

--- ClassPortfolio [Tues]/app.py
+++ ClassPortfolio [Tues]/app.py
@@ -3,7 +3,7 @@ This script runs the application using a development server.
 It contains the definition of routes and views for the application.
 """
 
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 # Make the WSGI interface available at the top level so wfastcgi can get it.
@@ -12,8 +12,8 @@ wsgi_app = app.wsgi_app
 
 @app.route('/')
 def hello():
-    """Renders a sample page."""
-    return "Hello World!"
+    """Renders a the main page with links to each dictionary."""
+    return render_template("index.html")
 
 if __name__ == '__main__':
     import os
